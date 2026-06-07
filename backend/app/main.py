1074d5e2eaaa669e1165e99530d3590971fffab5
yafisulthon <yafisulthon@gmail.com>
2026-06-07 22:20:32 +0700

    fix(cors): allow all origins for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 # ─── CORS ──────────────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"], # Mengizinkan semua origin (localhost & vercel)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
